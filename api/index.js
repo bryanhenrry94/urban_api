99315db536b2d491a16bb519cea31ee8cebaa71b
Bryan Navarrete <bryanhenrry94@gmail.com>
2025-01-13 22:55:39 -0500

    ajuste para que sea compatible con vercel

--- api/index.js
+++ api/index.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('./config/db');
+const mongoose = require('../config/db');
 const cors = require('cors');
 const morgan = require("morgan");
 
 // Routes
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const companyRoutes = require('./routes/companyRoutes');
-const personRoutes = require('./routes/personRoutes');
-const productRoutes = require('./routes/productRoutes');
-const invoiceRoutes = require('./routes/invoiceRoutes');
+const authRoutes = require('../routes/authRoutes');
+const userRoutes = require('../routes/userRoutes');
+const companyRoutes = require('../routes/companyRoutes');
+const personRoutes = require('../routes/personRoutes');
+const productRoutes = require('../routes/productRoutes');
+const invoiceRoutes = require('../routes/invoiceRoutes');
 
-const itemRoutes = require('./routes/itemRoutes');
-const houseRoutes = require('./routes/houseRoutes');
-const tenantRoutes = require('./routes/tenantRoutes');
+const itemRoutes = require('../routes/itemRoutes');
+const houseRoutes = require('../routes/houseRoutes');
+const tenantRoutes = require('../routes/tenantRoutes');
 
 const app = express();
 
@@ -24,6 +24,9 @@ app.use(express.json());
 app.use(morgan("combined"));
 
 // Routes v1
+app.get('/', function (req, res) {
+    res.send({ message: 'Welcome to Urban API' });
+})
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/companies', companyRoutes);
@@ -38,3 +41,5 @@ app.use('/api/tenants', tenantRoutes);
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
